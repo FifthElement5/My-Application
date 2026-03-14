f80050bb8134b3846a573cf8cc734c25d3b42dea
Fifth <i.m.galazewska@gmail.com>
2026-03-14 06:34:22 +0100

    first

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    View.OnClickListener sluchaczCyfr = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //sorawdzme co kliknęlo
+            Button b = (Button) v;
+            String dopisanaCyfra = b.getText().toString();
+
+            //pobieramy to co już jest na ekranie
+            String obecnytekst = ekran.getText().toString();
+
+            // dopisywanie
+            if (obecnytekst.equals(("0"))) {
+                ekran.setText(dopisanaCyfra);
+            } else {
+                ekran.setText( obecnytekst+ dopisanaCyfra);
+            }
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +45,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        TextView ekran = findViewById(R.id.result_tv);
+
+//        Button btn8 = findViewById(R.id.button10);
+//        btn8.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ekran.setText("8");
+//            }
+//        });
+//        Button btn7 = findViewById(R.id.button9);
+//        btn7.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ekran.setText("7");
+//            }
+//        });
+//
+//        Button btn9 = findViewById(R.id.button11);
+//        btn9.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ekran.setText("9");
+//            }
+//        });
+
+
+
+
+
+
+
     }
 }
